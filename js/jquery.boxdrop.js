e82b539fb3d4bd9ea50c3c7862a6a92aa94de5d5
Samarjeet <samarjeet2702@gmail.com>
2015-02-08 16:18:10 +0530

    Convert to jQuery plugin

---
+++ js/jquery.boxdrop.js
@@ -0,0 +1,31 @@
+(function ($){
+	// animate box
+	function toggleSlide($elem){
+		// check if it's visible
+		if($elem.css('display') == 'none'){
+			$elem.slideDown();
+		} else {
+			$elem.slideUp();
+		}
+	}
+
+	$.fn.boxdrop = function (opts) {
+		var me = this;
+		var getDrop = function(me){
+			return $(me).find('.box-title').find('.drop-image');
+		}
+
+		this.each(function(){
+			var $image = getDrop(this);
+			// handle click event
+			$image.on('click', function(event){
+				// item index
+				var i = getDrop(me).index($(this));
+				// corresponding box-content
+				var $box = $(me).find('.box-content');
+				toggleSlide($($box[i]));
+			});
+		});
+	}
+
+}) (jQuery);
