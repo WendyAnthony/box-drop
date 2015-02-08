7a113ec2934326cca8e841af38d79851048f6ed7
Samarjeet <samarjeet2702@gmail.com>
2015-02-08 10:00:58 +0530

    Add Some comments

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 (function ($){
+	// animate box
 	function toggleSlide($elem){
+		// check if it's visible
 		if($elem.css('display') == 'none'){
 			$elem.slideDown();
 		} else {
 			$elem.slideUp();
 		}
 	}
+
 	$('.drop-image').on('click', function(event){
+		// item index
 		var i = $('.drop-image').index($(this));
+		// corresponding box-content
 		var $box = jQuery($('.box-content')[i]);
 		toggleSlide($box);
 	});
