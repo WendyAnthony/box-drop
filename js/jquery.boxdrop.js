0fcd50c8f6a51c6a33c7e613c0f3a6d0990ca30d
Samarjeet <samarjeet2702@gmail.com>
2015-02-09 15:49:42 +0530

    Use Better Code

--- js/jquery.boxdrop.js
+++ js/jquery.boxdrop.js
@@ -1,33 +1,16 @@
-(function ($){
-	// animate box
-	function toggleSlide($elem){
-		// check if it's visible
-		if($elem.css('display') == 'none'){
-			$elem.slideDown();
-		} else {
-			$elem.slideUp();
-		}
-	}
-
-	$.fn.boxdrop = function (drop) {
-		drop = drop || toggleSlide;
-
-		var me = this;
-		var getDrop = function(me){
-			return $(me).find('.box-title').find('.drop-image');
-		}
-
-		return this.each(function(){
-			var $image = getDrop(this);
-			// handle click event
-			$image.on('click', function(event){
-				// item index
-				var i = getDrop(me).index($(this));
-				// corresponding box-content
-				var $box = $(me).find('.box-content');
-				drop($($box[i]));
-			});
-		});
-	}
-
-}) (jQuery);
+(function ($) {
+    // animate box
+    function toggleSlide($elem) {
+        // check if it's visible
+        $elem.slideToggle();
+    }
+    $.fn.boxdrop = function (drop) {
+        drop = drop || toggleSlide;
+        return this.each(function () {
+            $(this).on('click', '.drop-image', function(event) {
+                event.preventDefault();
+                drop($(this).closest('.box').find('.box-content'));
+            });
+        });
+    }
+})(jQuery);
